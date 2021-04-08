2311c1e5ace983cb474ccf9dfe658d44cd0bd057
Eva <eua_92@hotmail.com>
2021-04-08 23:43:48 +0300

    Fourth commit

---
+++ src/shared/tours.js
@@ -0,0 +1,63 @@
+export const TOURS = [
+    {
+        id: 0,
+        name: "Amsterdam City Center",
+        image: "assets/images/amsterdam-tour.jpg",
+        alt: "Amsterdam City",
+        duration: "3 hrs",
+        level: "Easy",
+        distance: "12km/7mi",
+        price: "50"
+    },
+    {
+        id: 1,
+        name: "Amsterdam - Utrecht",
+        image: "assets/images/utrecht-tour.jpg",
+        alt: "Castle De Haar in Utrecht",
+        duration: "3 days",
+        level: "Easy",
+        distance: "110km/70mi",
+        price: "360"
+    },
+    {
+        id: 2,
+        name: "Toulip Fields",
+        image: "assets/images/tulip-tour.jpg",
+        alt: "Tulip Fields in Netherlands",
+        duration: "4 days",
+        level: "Easy",
+        distance: "120km/75mi",
+        price: "450"
+    },
+    {
+        id: 3,
+        name: "North Holland",
+        image: "assets/images/north-holland-tour.jpg",
+        alt: "Volendam in North Holland",
+        duration: "6 days",
+        level: "Moderate",
+        distance: "136km/85mi",
+        price: "720"
+    },
+    {
+        id: 4,
+        name: "Amsterdam - Bruges",
+        image: "assets/images/bruges-tour.jpg",
+        alt: "Bruges Canal",
+        duration: "1 week",
+        level: "Moderate",
+        distance: "360km/225mi",
+        price: "980"
+    },
+    {
+        id: 5,
+        name: "Amsterdam - Maastricht",
+        image: "assets/images/maastricht-tour.jpg",
+        alt: "Maastricht Canal",
+        duration: "1 week",
+        level: "Moderate",
+        distance: "330km/206mi",
+        price: "870"
+    }
+
+]
